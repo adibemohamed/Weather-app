db19a54eaf07dfc0e0180733696695d8c3a29775
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-23 00:00:24 +0000

    add vue ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Klima-Weather app</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/main.css">
     <script src="/js/app.js" defer></script>
 </head>
 <body class="bg-blue-500">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat ullam nobis porro voluptates, maiores esse sit blanditiis. Hic qui debitis nobis ullam vero at odit sint asperiores. Consectetur itaque similique vero quasi voluptatibus, ad dolorum cupiditate reiciendis neque laudantium quidem.
-</body>
+    
+ </body>
 </html>
