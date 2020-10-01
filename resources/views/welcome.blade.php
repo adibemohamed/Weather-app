51d4fdb114da4af56c866b39b2e7ba44d8b768f7
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-10-01 23:28:13 +0000

    Done to deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Klima-Weather app</title>
+    <title>Fresh Weather</title>
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/weather-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0"></script> 
